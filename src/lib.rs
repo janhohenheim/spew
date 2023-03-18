5bc0695372a7022c1d92b9b0aabd6e4fdc6dcf9a
Jan Hohenheim <jan@hohenheim.ch>
2023-03-18 19:55:57 +0100

    Implement adding multiple spawners as tuples

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 
 use bevy::ecs::system::SystemState;
 use bevy::prelude::*;
+use bevy::utils::all_tuples;
 
 /// Everything you need to get started
 pub mod prelude {
@@ -40,32 +41,52 @@ pub trait SpewApp {
 
 impl SpewApp for App {
     fn add_spawners<T: Spawners>(&mut self, spawners: T) -> &mut App {
-        spawners.add_to_app(self)
+        spawners.add_to_app(self);
+        self
     }
 }
 
 pub trait Spawners {
-    fn add_to_app(self, app: &mut App) -> &mut App;
+    fn add_to_app(self, app: &mut App);
 }
 
-impl<T: Eq + Send + Sync + 'static, F: FnMut(Transform, &mut World) + 'static + Send + Sync>
-    Spawners for (T, F)
+pub trait Spawner {
+    fn add_to_app(self, app: &mut App);
+}
+
+impl<T: Eq + Send + Sync + 'static, F: FnMut(Transform, &mut World) + 'static + Send + Sync> Spawner
+    for (T, F)
 {
-    fn add_to_app(self, app: &mut App) -> &mut App {
-        let (key, mut spawner) = self;
+    fn add_to_app(self, app: &mut App) {
+        let (object, mut spawn_function) = self;
         let system = move |world: &mut World| {
             let mut event_system_state = SystemState::<EventReader<SpawnEvent<T>>>::new(world);
             let mut events = event_system_state.get_mut(world);
             let transforms: Vec<_> = events
                 .iter()
-                .filter(|event| event.object == key)
+                .filter(|event| event.object == object)
                 .map(|event| event.transform)
                 .collect();
             for transform in transforms {
-                spawner(transform, world);
+                spawn_function(transform, world);
             }
         };
         app.add_system(system);
-        app
     }
 }
+
+macro_rules! impl_spawners_tuples {
+    ($($name: ident),*) => {
+        impl<$($name),*> Spawners for ($($name,)*)
+            where
+                $($name: Spawner),*{
+                #[allow(non_snake_case, unused_variables)]
+                fn add_to_app(self, app: &mut App) {
+                    let ($($name,)*) = self;
+                    $($name.add_to_app(app);)*
+                }
+            }
+    }
+}
+
+all_tuples!(impl_spawners_tuples, 0, 15, S);
