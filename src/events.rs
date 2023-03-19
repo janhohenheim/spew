9f50791ab5475222bbd077bdd9096748099c44b8
Jan Hohenheim <jan@hohenheim.ch>
2023-03-19 21:02:23 +0100

    Make clippy happy

--- src/events.rs
+++ src/events.rs
@@ -57,6 +57,7 @@ where
 /// A trait that allows creating a [`SpawnEvent`] without user-provided data.
 pub trait NewSpawnEventWithoutData<T> {
     /// Create a new spawn event
+    #[allow(clippy::new-ret-no-self)]
     fn new(object: T) -> SpawnEvent<T, ()>
     where
         T: Eq + Send + Sync + 'static;
